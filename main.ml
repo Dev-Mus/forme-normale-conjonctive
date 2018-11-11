e97348d6516f36bc824fc1ae4af8dc2ab572f4ce
Version Beta <haithamkanuaa3@GMAIL.COM>
2018-11-11 21:21:03 +0100

    init projet

---
+++ main.ml
@@ -0,0 +1,19 @@
+open Prop_def;;
+ 
+open List;;
+open Prop_lexer;;
+ 
+
+let boucle in_channel =
+let lexbuffer = Lexing.from_channel in_channel in
+let lire_prop_expr () =
+  Prop_parser.programme Prop_lexer.token lexbuffer in
+ 
+	let p = lire_prop_expr () in print_term p;
+ 
+
+exit 0;;
+
+boucle stdin;;
+
+
