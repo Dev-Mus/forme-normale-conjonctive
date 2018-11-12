2548bc99bbdcc7fa954735b5d90859c4622bc1ab
Version Beta <haithamkanuaa3@GMAIL.COM>
2018-11-12 19:59:44 +0100

    change affichage \n ***

--- main.ml
+++ main.ml
@@ -5,14 +5,13 @@ open Prop_lexer;;
  
 
 let boucle in_channel =
-let lexbuffer = Lexing.from_channel in_channel in
-let lire_prop_expr () =
-  Prop_parser.programme Prop_lexer.token lexbuffer in
- 
-	let p = lire_prop_expr () in print_term p;
- 
-
-exit 0;;
+	let lexbuffer = Lexing.from_channel in_channel in
+		let lire_prop_expr () = 
+		Prop_parser.programme Prop_lexer.token lexbuffer in
+			let p = lire_prop_expr () in 
+				let k a= 			
+			  	print_string "\n\t**********\n"; 						in k (print_term p) ;
+	exit 0;;
 
 boucle stdin;;
 
