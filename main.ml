34b45558d94c384549466d40bb4de7cffcde3a1e
Version Beta <haithamkanuaa3@GMAIL.COM>
2018-11-13 18:39:38 +0100

    modifie main.ml 1er creation function FNC

--- main.ml
+++ main.ml
@@ -2,6 +2,42 @@ open Prop_def;;
  
 open List;;
 open Prop_lexer;;
+
+
+let rec fnc  term =
+	match term with
+		(Var x) 		 ->  
+						 print_string x
+		|Vrai 			 ->      
+						 print_string "vrai";
+		|Faux  			 ->     
+						 print_string "faux";
+		|OU(x,y) 		 ->   (
+             print_string "(";
+             print_term x;
+             print_string "#";
+             print_term y;
+             print_string ")"; 
+						)             
+		|ET(x,y)  	 ->	(
+             print_string "("; 
+             print_term x;
+             print_string "&";
+             print_term y;
+             print_string ")"; 
+						)             
+		|IMPLIQ(x,y) -> (
+             print_string "("; 
+             print_term x;
+             print_string "->";
+             print_term y;
+             print_string ")";
+						)             
+		|NEG x			 -> (
+						 print_string "~"; 
+						 print_term x
+						)
+;;
  
 
 let boucle in_channel =
@@ -9,10 +45,15 @@ let boucle in_channel =
 		let lire_prop_expr () = 
 		Prop_parser.programme Prop_lexer.token lexbuffer in
 			let p = lire_prop_expr () in 
-				let k a= 			
-			  	print_string "\n\t**********\n"; 						in k (print_term p) ;
+				let k p = 			
+			  print_string "\n\t**********\n";
+				print_term p;
+		  	print_string "\n\t**********\n";	in 
+					k p ;
 	exit 0;;
 
 boucle stdin;;
+
+
 
 
